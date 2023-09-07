9e561b40c049efe10ff342719279365c2b79332a
RHEZUS <ludivinseunda2@gmail.com>
2023-09-07 06:19:44 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDERR_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
