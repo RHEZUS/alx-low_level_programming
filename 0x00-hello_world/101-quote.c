ee175acf072455010c01ec1b5f630616d78e631e
RHEZUS <ludivinseunda2@gmail.com>
2023-09-07 05:27:08 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	putshar("","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
