59ce553f1b700b50b206e3cdfcf7efc154afd709
RHEZUS <ludivinseunda2@gmail.com>
2023-09-07 05:04:19 -0700

    try fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("\n", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d \n", sizeof(char), "byte(s)");
-	printf("Size of an int: %d \n", sizeof(int), "byte(s)");
-	printf("Size of a long int:  %d \n", sizeof(long int), "byte(s)");
-	printf("Size of a long long: %d \n", sizeof(long long int), "byte(s)");
-	printf("Size of a float: %d \n", sizeof(float), "byte(s)");
+	printf("Size of a char: %d %s \n", sizeof(char), "byte(s)");
+	printf("Size of an int: %d %s \n", sizeof(int), "byte(s)");
+	printf("Size of a long int:  %d %s \n", sizeof(long int), "byte(s)");
+	printf("Size of a long long: %d %s \n", sizeof(long long int), "byte(s)");
+	printf("Size of a float: %d %s \n", sizeof(float), "byte(s)");
 	return (0);
 }
 
