f0b02bd2d2a32682a7c94999697562e09cc984fb
RHEZUS <ludivinseunda2@gmail.com>
2023-09-08 06:21:56 -0700

    almost

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,23 +20,20 @@ int main(void)
 	
 			for (k = j + 1; k <= 9; k++)
 			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				if (i == 7 && j == 8 && k == 9)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
+					putchar('\n');
 				}
 				else
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
