8a6e37f8a19343f3ef6d30e7c01fe26205683765
RHEZUS <ludivinseunda2@gmail.com>
2023-09-08 06:15:46 -0700

    almost

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,20 +13,18 @@ int main(void)
 
 		for (j = i + 1; j <= 9; j++)
 		{
+			putchar(i + '0');
+			putchar(j + '0');
 			if (i == 8 && j == 9)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
+				putchar('\n');
 			}
 			else
 			{
-				putchar(i + '0');
-				putchar(j + '0');
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
