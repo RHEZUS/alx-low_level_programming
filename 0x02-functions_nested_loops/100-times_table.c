721eb94c194b6afd4e0a02466107fd6dda4e9051
RHEZUS <ludivinseunda2@gmail.com>
2023-09-12 08:45:59 -0700

    Ready for the checker

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,8 +16,8 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				if((j <= 0))
-					 printf("%d", result);
+				if ((j <= 0))
+					printf("%d", result);
 				else if ((result < 10))
 					printf("   %d", result);
 				else if ((result < 100))
