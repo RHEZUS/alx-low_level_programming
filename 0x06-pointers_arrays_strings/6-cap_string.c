735896810fb4a0ac1c1e69a4969e8d9c1a0f0314
RHEZUS <ludivinseunda2@gmail.com>
2023-09-21 05:24:36 -0700

    solved

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -37,9 +37,11 @@ int isSpecialChar(char c)
 char *cap_string(char *s)
 {
 	int i = 0;
-
+	
 	while (s[i] != '\0')
 	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 		if (isSpecialChar(s[i]))
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
