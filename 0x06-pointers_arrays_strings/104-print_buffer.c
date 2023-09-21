ce99823d3d0c0e80e03147354beda3becf364724
RHEZUS <ludivinseunda2@gmail.com>
2023-09-21 13:41:38 -0700

    check

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,59 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
 
 /**
  * print_buffer - prints a buffer
- * @b: the buffer to print
- * @size: the size of the buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
  */
-
-#include <stdio.h>
-
 void print_buffer(char *b, int size)
 {
-	if (size <= 0)
+	int i;
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("\n");
-		return;
-	}
-	for (int i = 0; i < size; i += 10)
-	{
-		printf("%.8x ", i);
-		for (int j = i; j < i + 10; j++)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if (j < size)
-				printf("%.2x", (unsigned char)b[j]);
-			else
-				printf("  ");
-			if (j % 2 == 1)
-				printf(" ");
+			print_line(b, 9, i);
 		}
-		printf(" ");
-		for (int j = i; j < i + 10; j++)
+		else
 		{
-			if (j < size)
-			{
-				if (b[j] >= 32 && b[j] <= 126)
-					printf("%c", b[j]);
-				else
-					printf(".");
-			}
-			else
-				printf(" ");
+			print_line(b, size % 10 - 1, i);
 		}
-		printf("\n");
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
