ba239cc787310499113c310425bcf69605c5f045
RHEZUS <ludivinseunda2@gmail.com>
2023-09-20 23:48:45 -0700

    3-strcmp.c solved

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,13 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (s1 == s2))
 	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
 		s1++;
 		s2++;
 	}
 
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (*s1 - *s2);
 }
