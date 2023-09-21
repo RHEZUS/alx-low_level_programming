7ba6403f18cb51514035759545f4a0c3184bbb9d
RHEZUS <ludivinseunda2@gmail.com>
2023-09-21 00:18:19 -0700

    solve 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,19 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j = 0;
+	char c [] = "aAeEoOtTlL";
+	char cd [] = "4433007711";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		while (c != '\0')
+		{
+			if (s[i] == c[j])
+				s[i] = cd[j];
+			j++
+		}
+		j = 0;
 		i++;
 	}
 	return (s);
