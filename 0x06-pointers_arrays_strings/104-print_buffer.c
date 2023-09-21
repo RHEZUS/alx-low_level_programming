cfb586a27a5aa22742a69ac9a5216e0d09516e50
RHEZUS <ludivinseunda2@gmail.com>
2023-09-21 10:27:00 -0700

    almost done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,7 +6,40 @@
  * @size: the size of the buffer
  */
 
+#include <stdio.h>
+
 void print_buffer(char *b, int size)
 {
-
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < size; i += 10)
+	{
+		printf("%.8x ", i);
+		for (int j = i; j < i + 10; j++)
+		{
+			if (j < size)
+				printf("%.2x", (unsigned char)b[j]);
+			else
+				printf("  ");
+			if (j % 2 == 1)
+				printf(" ");
+		}
+		printf(" ");
+		for (int j = i; j < i + 10; j++)
+		{
+			if (j < size)
+			{
+				if (b[j] >= 32 && b[j] <= 126)
+					printf("%c", b[j]);
+				else
+					printf(".");
+			}
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
 }
