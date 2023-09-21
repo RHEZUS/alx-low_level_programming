f72adb753fb566f9e8a907c30e6adef830ad5b9e
RHEZUS <ludivinseunda2@gmail.com>
2023-09-20 23:25:26 -0700

    Try2: fix 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -27,7 +27,7 @@ char *_strncat(char *dest, char *src, int n)
 	int destLen = _strlen(dest);
 	int i = 0;
 
-	while (isrc[i] != '\0' && < n)
+	while (dest[i] != '\0' && i < n)
 	{
 		dest[destLen + i] = src[i];
 		i++;
