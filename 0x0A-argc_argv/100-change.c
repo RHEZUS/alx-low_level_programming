b326f818449f7eecbaf46a6ae633f3434ddac18e
RHEZUS <ludivinseunda2@gmail.com>
2023-10-02 05:13:31 -0700

    no argument error solved

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,14 +12,16 @@ int main(int argc, char *argv[])
 {
 	int coins[] = {25, 10, 5, 2, 1};
 	int i = 0;
-	int cents = atoi(argv[1]);
+	int cents;
 	int c = 0;
 
-	if ((argc - 1) != 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
 
 	if (cents <= 0)
 	{
