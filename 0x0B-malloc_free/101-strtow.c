d986c0747f3b53eeda780b421a70f93efc46a0e9
RHEZUS <ludivinseunda2@gmail.com>
2023-10-04 07:58:47 -0700

    101 betty style corrected

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-char *util (int start,int end, char *str);
+char *util(int start, int end, char *str);
 int strArraylen(char *str);
 
 /**
@@ -11,7 +11,7 @@ int strArraylen(char *str);
 char **strtow(char *str)
 {
 	char **strArr;
-	int i = 0, j = 0, len = 0 , start = 0, end = 0;
+	int i = 0, j = 0, len = 0, start = 0, end = 0;
 
 	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
@@ -20,7 +20,7 @@ char **strtow(char *str)
 
 	if (strArr == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 	while (str[i] != '\0')
@@ -36,15 +36,22 @@ char **strtow(char *str)
 		}
 		i++;
 	}
-	return (strArr);	
+	return (strArr);
 }
 
+
+
+/**
+ * strArraylen - finds the number of words in a string
+ * @str: the string
+ * Return: the number of words
+ */
 
 int strArraylen(char *str)
 {
 	int i = 0, len = 0;
 
-	while(str[i++] != '\0')
+	while (str[i++] != '\0')
 	{
 		if (str[i] == ' ' && str[i - 1] != ' ')
 			len += 1;
@@ -52,22 +59,32 @@ int strArraylen(char *str)
 	return (len);
 }
 
-char *util (int start,int end, char *str)
+
+/**
+ * util - splits the words in a string and return a nointer to memory
+ * @start: the index where the word starts
+ * @end: where it ends
+ * @str:  the string
+ * Return: the splitted word
+ */
+
+char *util(int start, int end, char *str)
 {
-	int i = 0, j = 0, len=0;
+	int i = 0, j = 0, len = 0;
 	char *word;
+
 	len = (end - start) + 1;
 
 	word = (char *)malloc(len * sizeof(char *));
 	if (word == NULL)
 		return (NULL);
-	
-	for(i = start; i < end; i++)
+
+	for (i = start; i < end; i++)
 	{
 		word[j] = str[i];
 		j++;
 	}
 
 	word[end] = '\0';
-	return(word);
+	return (word);
 }
