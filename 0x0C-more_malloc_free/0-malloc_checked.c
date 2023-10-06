383007e8696ef7c4a61f1d62b7f85d6f4a827bfb
RHEZUS <ludivinseunda2@gmail.com>
2023-10-06 02:35:13 -0700

    task 0 solved

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: the size of the memory
+ * Return: a pointer to the allocated memory or 98
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
