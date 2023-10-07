75e8556b6bb4aebd4809f1fc6b2d35fec4192574
RHEZUS <ludivinseunda2@gmail.com>
2023-10-07 00:09:13 -0700

    Fixed: betty style task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ptr;
 
-	if (nmemb == 0 || size ==0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 
