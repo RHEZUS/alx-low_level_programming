4958d32312e5c0782cb5402e28cd0a247cc4928b
RHEZUS <ludivinseunda2@gmail.com>
2023-10-11 01:17:50 -0700

    Task 0 solved

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the pointer to the function that will print
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
