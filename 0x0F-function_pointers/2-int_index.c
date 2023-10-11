c18297b3cea5073b4e0b0edc3dc953604326aeb4
RHEZUS <ludivinseunda2@gmail.com>
2023-10-11 05:13:06 -0700

    All tasks completed and tested

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
