5bf1838c1a501f8887b54901fbf0d2682a0e035d
RHEZUS <ludivinseunda2@gmail.com>
2023-10-19 05:33:12 -0700

    try to solve 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+
+/**
+ * first - Apply the constructor attribute to myStartupFun() so that it
+ * is executed before main()
+ */
 void first(void) __attribute__((constructor));
 
 /**
@@ -7,6 +12,6 @@ void first(void) __attribute__((constructor));
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
