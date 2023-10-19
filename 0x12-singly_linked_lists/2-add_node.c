04d524c4d4c20e0a2be8cec4955dca1923af3719
RHEZUS <ludivinseunda2@gmail.com>
2023-10-19 05:29:55 -0700

    question 2 solved

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = (list_t *)malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
