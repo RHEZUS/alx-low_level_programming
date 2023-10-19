87762a17c624e1877e3d01c2b7e38ab48a38e1a1
RHEZUS <ludivinseunda2@gmail.com>
2023-10-19 10:35:06 -0700

    correct

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,17 @@
-	extern	printf		; the C function, to be called
+extern	printf		; the C function, to be called
+section .data
+    hello db "Hello, Holberton",0 ; Define the string to be printed
+    format db "%s",10,0           ; Define the format string with a newline character
 
-	        section .data		; Data section, initialized variables
-msg:		db "Hello, Holberton", 0 ; C string needs 0
-fmt:	    	db "%s", 10, 0          ; The printf format, "\n",'0'
+section .text
+    global main                   ; Entry point for the program
+    extern printf                ; Declare the printf function from the C library
 
-	        section .text		; Code section.
+main:
+    push rbp                      ; Save the base pointer
+    mov rdi, format               ; Load the address of the format string into rdi
+    mov rsi, hello                ; Load the address of the hello string into rsi
+    call printf                   ; Call the printf function to print the formatted string
+    pop rbp                       ; Restore the base pointer
+    ret                           ; Return from the main function
 
-	        global main		; the standard gcc entry point
-main:					; the program label for the entry point
-	        push    rbp		; set up stack frame, must be alligned
-
-		mov	rdi,fmt
-		mov	rsi,msg
-		mov	rax,0		; or can be  xor  rax,rax
-	        call    printf		; Call C function
-
-		pop	rbp		; restore stack
-
-		mov	rax,0		; normal, no error, return value
-		ret			; return
