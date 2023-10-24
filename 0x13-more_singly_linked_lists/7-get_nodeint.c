f1736999b9ab4b8dfec61289191ef8c470c9c090
RHEZUS <ludivinseunda2@gmail.com>
2023-10-24 07:49:40 +0300

    question 7 solved

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,12 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-
+	
+	if (head == NULL)
+		return (NULL);
 	for (i = 0; i <= index - 1; i++)
 	{
-		if (i < index && head == NULL)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
