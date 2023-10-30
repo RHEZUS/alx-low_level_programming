56d27e76683bfe05fc1a4511ac9a75440aeb2d1d
RHEZUS <ludivinseunda2@gmail.com>
2023-10-30 16:15:01 +0300

    Solve all third tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,23 +12,22 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
+	int file, flag = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	file  = open(filename, O_WRONLY | O_APPEND, 0600);
 
 	if (file == -1)
 		return (-1);
-
-	if (write(file, text_content, strlen(text_content) - 1) == -1)
-	{
-		close(file);
-		return (-1);
-	}
+	if (text_content != NULL)	
+		flag = write(file, text_content, strlen(text_content) - 1);
 
 	close(file);
-	return (1);
 
+	if (flag == -1)
+		return (-1);
+	
+	return (1);
 
 }
