47dbda91c029125a8721b7df86817f1bd250d755
RHEZUS <ludivinseunda2@gmail.com>
2023-10-30 15:52:37 +0300

    Solve all first tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	char *str = text_content == NULL ? "" : text_content;
+	int file, flag;
 
 	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	flag = write(file, text_content, strlen(text_content));
 
-	if (file == -1)
+	if (file == -1 || flag == -1)
 		return (-1);
-	if (write(file, str, strlen(str) - 1) == -1)
-	{
-		close(file);
-		return (-1);
-	}
 
+	close(file);
 	return (1);
 }
