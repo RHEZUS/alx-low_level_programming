c475c9aa4dfb1333a78d4f458144d7286fe09742
RHEZUS <ludivinseunda2@gmail.com>
2023-12-07 15:33:19 +0300

    correct betty style

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,7 +25,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for(i = 0; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (current == NULL)
 			return (-1);
