2aa547d39fbdcf6e2dfc2ee2761ffe4699fc9d09
RHEZUS <ludivinseunda2@gmail.com>
2023-12-07 16:04:25 +0300

    solve task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (i == idx)
 			{
 				if (head->next == NULL)
-					add_dnodeint_end(h, n);
+					new_node = add_dnodeint_end(h, n);
 				else
 				{
 					new_node = malloc(sizeof(dlistint_t));
