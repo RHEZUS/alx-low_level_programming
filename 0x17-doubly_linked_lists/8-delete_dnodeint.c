9387c255dbcb56ff42a55c474c56913a5a2ef460
RHEZUS <ludivinseunda2@gmail.com>
2023-12-07 15:32:14 +0300

    solve task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,14 +25,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (current != NULL && i < index)
+	for(i = 0; i < index; i++)
 	{
+		if (current == NULL)
+			return (-1);
 		current = current->next;
-		i++;
 	}
-
-	if (current == NULL)
-		return (-1);
 	current->prev->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = current->prev;
