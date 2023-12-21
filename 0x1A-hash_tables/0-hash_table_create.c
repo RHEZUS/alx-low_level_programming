ea9dc52b99893aa695b2358d87505b6756a9eeef
Seunda <ludivinseunda2@gmail.com>
2023-12-21 17:11:32 +0300

    solve tasks

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size){
+    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+    unsigned long int i = 0;
+    if (!hash_table)
+        return (NULL);
+    hash_table->size = size;
+    hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+    if (!hash_table->array)
+        return (NULL);
+    for (i = 0; i < size; i++)
+        hash_table->array[i] = NULL;
+    return (hash_table);
+}
