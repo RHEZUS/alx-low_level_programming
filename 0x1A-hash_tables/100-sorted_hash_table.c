753214392c71a55f745086f7cc6b0b3414caa372
Seunda <ludivinseunda2@gmail.com>
2023-12-22 16:07:01 +0300

    solve tasks

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,39 +1,73 @@
 #include "hash_tables.h"
 
 
+/**
+ * shash_table_create - creates a sorted hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to the new table, or NULL on failure
+ */
+
 shash_table_t *shash_table_create(unsigned long int size)
 {
-    shash_table_t *hash_table;
+	shash_table_t *hash_table;
 
-    hash_table = malloc(sizeof(shash_table_t));
+	hash_table = malloc(sizeof(shash_table_t));
 
-    if (hash_table == NULL)
-        return (0);
+	if (hash_table == NULL)
+		return (0);
 
-    hash_table->array = malloc(sizeof(shash_node_t));
-    if (hash_table->array == NULL)
-        return (NULL);
-    hash_table->size = size;
-    hash_table->shead = NULL;
-    hash_table->stail = NULL;
+	hash_table->array = malloc(sizeof(shash_node_t));
+	if (hash_table->array == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->shead = NULL;
+	hash_table->stail = NULL;
 
-    for (size_t i = 0; i < size; i++)
-    {
-        hash_table->array[i] = NULL;
-    }
-    return (hash_table);  
+	for (size_t i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
 }
+
+/**
+ * shash_table_set - sets a key to a value in the hash table
+ * @ht: sorted hash table
+ * @key: key to the data
+ * @value: data to add
+ *
+ * Return: 1 on success, 0 otherwise
+ */
+
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-    if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
 }
+
+
+/**
+ * shash_table_get - retrieve a value from the hash table
+ * @ht: hash table
+ * @key: key to the data
+ *
+ * Return: the value associated with key, or NULL on failure
+ */
+
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
 
 }
+
+/**
+ * shash_table_print - prints a sorted hash table
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
 void shash_table_print(const shash_table_t *ht)
 {
 	shash_node_t *current;
@@ -60,10 +94,26 @@ void shash_table_print(const shash_table_t *ht)
 	}
 	printf("}\n");
 }
+
+/**
+ * shash_table_print_rev - prints a sorted hash table in reverse
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
+
 void shash_table_print_rev(const shash_table_t *ht)
 {
 
 }
+
+/**
+ * shash_table_delete - deletes a sorted hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
+ */
+
 void shash_table_delete(shash_table_t *ht)
 {
 
