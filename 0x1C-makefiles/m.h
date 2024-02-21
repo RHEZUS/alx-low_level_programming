eb085ec71b67e04a60fb6c35f59ca06fa3097ec1
Seunda <ludivinseunda2@gmail.com>
2024-02-21 12:09:02 +0300

    solve mandatory tasks

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_school(void);
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+
+/**
+ * main - Entry point
+ * 
+ * Return: always success
+*/
+
+int main(void)
+{
+    print_school();
+    return (EXIT_SUCCESS);
+}
